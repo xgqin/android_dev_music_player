fe6ea4ffe8543a3b0e9ca0bba9b92ab4493300a6
QIN Xingguo <xgqin@guet.edu.cn>
2019-11-11 22:47:48 +0800

    Step 1: Accessing audio files with MediaStore provider.

---
+++ app/src/main/java/com/glriverside/xgqin/ggmusic/MainActivity.java
@@ -0,0 +1,86 @@
+package com.glriverside.xgqin.ggmusic;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ContentResolver mContentResolver;
+    private ListView mPlaylist;
+    private MediaCursorAdapter mCursorAdapter;
+
+    private final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    private final String SELECTION = MediaStore.Audio.Media.IS_MUSIC + " = ? " +
+            " AND " + MediaStore.Audio.Media.MIME_TYPE + " LIKE ? ";
+    private final String[] SELECTION_ARGS = {
+            Integer.toString(1),
+            "audio/mpeg"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPlaylist = findViewById(R.id.lv_playlist);
+
+        mContentResolver = getContentResolver();
+        mCursorAdapter = new MediaCursorAdapter(MainActivity.this);
+        mPlaylist.setAdapter(mCursorAdapter);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                //
+            } else {
+                requestPermissions(PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            }
+        } else {
+            initPlaylist();
+        }
+    }
+
+
+    private void initPlaylist() {
+        Cursor cursor = mContentResolver.query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                null,
+                SELECTION,
+                SELECTION_ARGS,
+                MediaStore.Audio.Media.DEFAULT_SORT_ORDER);
+
+        if (cursor != null) {
+            mCursorAdapter.swapCursor(cursor);
+            mCursorAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE:
+                if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initPlaylist();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/glriverside/xgqin/ggmusic/MediaCursorAdapter.java
@@ -0,0 +1,87 @@
+package com.glriverside.xgqin.ggmusic;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MediaCursorAdapter extends CursorAdapter {
+
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    private static final int NORMAL_LENGTH = 20;
+
+    private static final String TAG = MediaCursorAdapter.class.getSimpleName();
+
+    public MediaCursorAdapter(Context context) {
+        super(context, null, 0);
+        mContext = context;
+
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        View itemView = mLayoutInflater.inflate(R.layout.list_item, viewGroup, false);
+
+        if (itemView != null) {
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.tvTitle = itemView.findViewById(R.id.tv_title);
+            viewHolder.tvArtist = itemView.findViewById(R.id.tv_artist);
+            viewHolder.tvOrder = itemView.findViewById(R.id.tv_order);
+            viewHolder.divider = itemView.findViewById(R.id.divider);
+            itemView.setTag(viewHolder);
+
+            return itemView;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        int titleIndex = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+        int artistIndex = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+
+
+        String title = cursor.getString(titleIndex);
+        String artist = cursor.getString(artistIndex);
+
+
+        int titleLength = title.length();
+        int position = cursor.getPosition();
+        int count = cursor.getCount();
+
+        if (viewHolder != null) {
+            viewHolder.tvTitle.setText(title);
+            if (titleLength > NORMAL_LENGTH) {
+                String reTitle = title.substring(0, NORMAL_LENGTH/2) +
+                        "..." + title.substring(titleLength-NORMAL_LENGTH/2, titleLength);
+                viewHolder.tvTitle.setText(reTitle);
+            }
+
+            viewHolder.tvArtist.setText(artist);
+            viewHolder.tvOrder.setText(Integer.toString(position+1));
+            viewHolder.divider.setVisibility(View.VISIBLE);
+
+            if (position == count - 1) {
+                viewHolder.divider.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    public class ViewHolder {
+        TextView tvTitle;
+        TextView tvArtist;
+        TextView tvOrder;
+        View divider;
+    }
+}
